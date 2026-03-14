a2202bcaddd2b1a0abc1166e4eb7f8311949b590
camarottoMD <matheus.domingues2@estudante.ifms.edu.br>
2026-03-13 21:45:09 -0300

    implement stack data structure with invert method for reversing strings

---
+++ inversor.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class Stack:
+    def __init__(self): 
+        self.top = None
+        self._size = 0
+
+    def invert(self, data):
+        for letter in data:
+            node = Node(letter)
+            node.next = self.top
+            self.top = node
+            self._size += 1
+            pointer = self.top
+
+        #até aqui ele vai empilhar as letras
+        while self.top: #tenho que fazer ele percorrer a palavra inteira até acabar
+            print(self.top.data)
+            self.top = self.top.next
+            
+
+
+
+if __name__ == "__main__":
+    pilha = Stack()
+    palavra = input("Insira uma palavra")
+    pilha.invert(palavra)
